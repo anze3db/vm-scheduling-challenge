6b74292ca33c6b8638dc8561b6856a7b9d020bf1
Anže Pečar <anze@pecar.me>
2024-02-12 11:47:35 +0000

    Init db script

---
+++ init_db.py
@@ -0,0 +1,51 @@
+import asyncio
+import datetime as dt
+
+import psycopg
+
+
+def tomorrow(hour: int, minutes: int) -> dt.datetime:
+    tomorrow = dt.date.today() + dt.timedelta(days=1)
+    return dt.datetime(tomorrow.year, tomorrow.month, tomorrow.day, hour, minutes)
+
+
+async def create_exam_table():
+    async with await psycopg.AsyncConnection.connect(
+        "dbname=postgres user=postgres"
+    ) as aconn:
+        async with aconn.cursor() as acur:
+            await acur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS exam (
+                    "id" serial primary key,
+                    "name" text,
+                    "start" timestamp,
+                    "end" timestamp,
+                    "students" int,
+                    PRIMARY KEY ("id")
+                );
+
+                -- Allocated VMs for each student
+                CREATE TABLE IF NOT EXISTS exam_vm (
+                    "id" uuid,
+                    "exam_id" integer references exam(id)
+                );
+
+                TRUNCATE TABLE exam_vm, exam;
+                """
+            )
+            exams = [
+                ("Mathematics 101", tomorrow(10, 0), tomorrow(10, 45), 2500),
+                ("Physics 101", tomorrow(10, 0), tomorrow(11, 0), 2500),
+                ("Mathematics 201", tomorrow(11, 15), tomorrow(12, 0), 2500),
+                ("Physics 102", tomorrow(12, 0), tomorrow(13, 0), 2500),
+                ("History 102", tomorrow(15, 0), tomorrow(13, 0), 2500),
+            ]
+            await acur.executemany(
+                """INSERT INTO exam ("name", "start", "end", "students") VALUES(%s, %s, %s, %s);""",
+                exams,
+            )
+
+
+if __name__ == "__main__":
+    asyncio.run(create_exam_table())
