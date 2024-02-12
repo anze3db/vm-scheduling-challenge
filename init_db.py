c9afecacadd45714acfb84d5ec8ff3870cdea50d
Anže Pečar <anze@pecar.me>
2024-02-12 17:53:25 +0000

    Cleanup init_db

--- init_db.py
+++ init_db.py
@@ -1,17 +1,15 @@
 import asyncio
-import datetime as dt
+import logging
+import os
 
 import psycopg
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO").upper())
 
 CONNECTION_STR = "dbname=postgres user=postgres"
 
 
-def tomorrow(hour: int, minutes: int) -> dt.datetime:
-    tomorrow = dt.date.today() + dt.timedelta(days=1)
-    return dt.datetime(tomorrow.year, tomorrow.month, tomorrow.day, hour, minutes)
-
-
-async def create_exam_table():
+async def create_tables():
     async with await psycopg.AsyncConnection.connect(CONNECTION_STR) as aconn:
         async with aconn.cursor() as acur:
             await acur.execute(
@@ -32,23 +30,10 @@ async def create_exam_table():
                     created timestamp default now(),
                     ended timestamp default null
                 );
-
-                TRUNCATE TABLE exam_vm, exam;
                 """
             )
-            exams = [
-                ("Mathematics 101", tomorrow(10, 0), tomorrow(10, 45), 2400),
-                ("Physics 101", tomorrow(10, 0), tomorrow(11, 0), 2400),
-                ("Mathematics 201", tomorrow(11, 15), tomorrow(12, 0), 2400),
-                ("Physics 102", tomorrow(12, 0), tomorrow(13, 0), 2400),
-                ("History 102", tomorrow(15, 0), tomorrow(13, 0), 2400),
-                ("History 103", tomorrow(17, 0), tomorrow(13, 0), 2400),
-            ]
-            await acur.executemany(
-                """INSERT INTO exam ("name", "start", "end", "students") VALUES(%s, %s, %s, %s);""",
-                exams,
-            )
+    logging.info("Done!")
 
 
 if __name__ == "__main__":
-    asyncio.run(create_exam_table())
+    asyncio.run(create_tables())
